2741bb06950a6d9b4cfaa0f41c85f565d8581f0a
shortstack-applications <46460111+shortstack-applications@users.noreply.github.com>
2020-03-18 14:57:45 +0000

    Made some small amendments
    Made some small amendments and tidied up code.

--- index.js
+++ index.js
@@ -1,132 +1,117 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/*import {Button} from './Button';
-import {Board} from './Board';*/
 
-const colours = ['green', 'blue', 'red', 'purple'];
-
-//Coloured buttons
-function Button(props) {
+function Square(props) {
     return (
         <button 
-            className={props.value}
+            className="square" 
             onClick={props.onClick}
         >
-            
+            {props.value}
         </button>
         );
     }
 
-//Checks running score and return Winner when total is met
-function checkScore(score) {
-    let status;
-    if (score == "10") {
-        return status = "Winner!";
-        }
-    }
-
-
-// Board is UI of game
-export class Board extends React.Component {
+class Board extends React.Component {
     constructor(props) {
         super(props);
-        
         this.state = {
-            runningTotal: 0,
-            colourMatch: colours[0],
-            randomColours: colours.sort(function(a, b){return 0.5 - Math.random()}),
+            squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     
-    //Calles Button function
-    renderButton(i) {
-        return (
-            <Button
-                value={this.state.randomColours[i]}
-                onClick={() => this.handleClick(this.state.randomColours[i])}
-            />
-        );
-    }
-
     handleClick(i) {
-        let status = "";
-        let randInt = Math.floor(Math.random() * Math.floor(4));
-        let runningTotal;
-        
-        if (i == this.state.colourMatch) {
-            let randomColours = colours.sort(function(a, b){return 0.5 - Math.random()});
-            runningTotal = this.state.runningTotal + 1; 
-
-            this.setState({
-                runningTotal: runningTotal,
-                randomColours: randomColours,
-                colourMatch: randomColours[randInt]
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';;
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
             });
-        } else {
-            let randomColours = colours.sort(function(a, b){return 0.5 - Math.random()});
-            let runningTotal = 0; 
-            
-            this.setState({
-                runningTotal: runningTotal,
-                randomColours: randomColours,
-                colourMatch: randomColours[randInt]
-                });
-            }
+    }
+    
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+                />
+            );
         }
     
     render() {
-        const status = checkScore(this.state.runningTotal);
-        if (status == "Winner!") {
-            this.state.runningTotal = 0
+        const winner = calculateWinner(this.state.squares);
+        const status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
         
         return (
-            <div>
-                <div>
-                    <p>Match the colour to the name!</p>
+                  <div>
+        <div className="status">{status}</div>
+                <div className="board-row">
+                  {this.renderSquare(0)}
+                  {this.renderSquare(1)}
+                  {this.renderSquare(2)}
                 </div>
-                <div className="colourMatch">
-                    <p id="match">{this.state.colourMatch}</p>
-                </div>                
-                <div className="buttonRow">
-                    {this.renderButton(0)}
-                    {this.renderButton(1)}
-                </div>                
-                <div className="buttonRow">
-                    {this.renderButton(2)}
-                    {this.renderButton(3)}
-                </div>                
-                <div className="scoreCount">
-                    <p>Score: {this.state.runningTotal}</p>
-                    <p>{status}</p>
+                <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
                 </div>
-                
-            </div>
-        )
+                <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+                </div>
+        </div>
+        );
     }
 }
-
-
-
 
 class Game extends React.Component {
     render() {
         return (
             <div className="game">
-                <div className="board-container">
-                    <Board/>
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{ /*To DO */}</ol>
                 </div>
             </div>
-        )
+        );
     }
 }
-
-// ========================================
-
-// TODO!! Increment total, re render squares on click. 
 
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i=0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
